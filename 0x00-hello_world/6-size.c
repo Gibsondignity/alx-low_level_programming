eac374f6b48f0667bf5879c75ba384f800f0f567
Gibson Dignity <Gibson Dignity>
2022-06-30 16:58:31 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int main(void)
 	long long int e = 2000000000;
 
 
-	printf("Size of a char: %d\n", sizeof(b));
-	printf("Size of an int: %d\n", sizeof(a));
-	printf("Size of a long int: %d\n", sizeof(d));
-	printf("Size of a long long int: %d\n", sizeof(e));
-	printf("Size of a float: %d\n", sizeof(c));
+	printf("Size of a char: %d byte(s)\n", sizeof(b));
+	printf("Size of an int: %d byte(s)\n", sizeof(a));
+	printf("Size of a long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(e));
+	printf("Size of a float: %d byte(s)\n", sizeof(c));
 
     	return (0);
 }
