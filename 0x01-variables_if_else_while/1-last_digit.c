43e2ddd730217927935b9ca3b142487b3457e94b
Gibson Dignity <Gibson Dignity>
2022-07-01 13:44:44 -0700

    find the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,20 +18,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-	char words = "Last digit of";
-	char last_words = "and is less than 6 and not 0"
 
 	if (lastDigit > 5)
 	{
-		printf("%d %d is %d and is greater than 5\n", words, n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	if (lastDigit == 0)
 	{
-		printf("%d %d is %d and is 0\n", words, n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("%d %d is %d %d\n", words, n, lastDigit, last_words);
+		printf("Last digit of %d is %d \n", n, lastDigit);
 	}
 
 
