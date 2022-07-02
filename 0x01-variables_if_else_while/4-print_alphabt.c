9024531bed1948c786355584c991bac69c238a9d
Gibson Dignity <Gibson Dignity>
2022-07-01 17:21:57 -0700

    all alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != 'q' or al != 'e')
+		{
+		putchar(al);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
